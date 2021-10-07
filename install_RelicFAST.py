ff2dcc1729b8e2287162c909507f167d0803a4ba
Nicholas DePorzio <nicholasdeporzio@g.harvard.edu>
2021-10-07 10:54:23 -0400

    Updated RelicFast installation script for personal setup.

--- install_RelicFAST.py
+++ install_RelicFAST.py
@@ -32,7 +32,11 @@ reading_file = open("./Makefile", "r")
 new_file_content = ""
 for line in reading_file:
   stripped_line = line.strip()
-  new_line = stripped_line.replace("F90C     = ifort", "F90C     = gfortran")
+  new_line = stripped_line.replace(
+    "F90C     = ifort", "F90C     = gfortran"
+  ).replace(
+    "OMPFLAG   = -openmp", ""
+  )
   new_file_content += new_line +"\n"
 reading_file.close()
 os.system('rm ./Makefile') 
