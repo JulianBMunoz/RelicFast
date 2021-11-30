d683d70b5e8d99b36f5e23235aab110556b82c41
Nicholas DePorzio <nicholasdeporzio@g.harvard.edu>
2021-11-30 14:01:56 -0500

    Update script showing how RelicFast computes eulerian bias from lagrangian bias.

--- scripts/RelicFast_Lagrangian_to_Eulerian_Bias.py
+++ scripts/RelicFast_Lagrangian_to_Eulerian_Bias.py
@@ -24,7 +24,7 @@ kmax = 1.5
 Nk = 50
 
 h_lcdm = 0.67
-omega_nu = Mnu/93.14
+omega_nu = Mnu/93.2
 omega_cdm_LCDM = 0.12
 omega_b_LCDM = 0.022
 #Omega_M_LCDM = 0.27464
@@ -92,6 +92,8 @@ for m_idx, m_val in enumerate(Mnu):
             "omegab = 0.02226", "omegab = "+f'{omega_b_LCDM:.3f}'
         ).replace(
             "omegac = 0.11271", "omegac = "+f'{omega_cdm_LCDM:.3f}'
+        ).replace(
+            "Mhalo_min = 1e13", "Mhalo_min = "+f'{1e13/h_lcdm:.3e}'
         )
         if (m_val!=0.0): 
             new_line = new_line.replace(
@@ -106,11 +108,21 @@ for m_idx, m_val in enumerate(Mnu):
     os.system('./relicfast run.ini')
     
     # Collect bias for requested redshift 
+#    axioncamb_data_eulbias.append(
+#        np.loadtxt(rfpath_outputsuffix+'bias_Euler_z'+f'{redshift:.2f}'+'_M13.48_Nk50.dat', skiprows=1)
+#    )
+#    axioncamb_data_lagbias.append(
+#        np.loadtxt(rfpath_outputsuffix+'bias_Lagrangian_z'+f'{redshift:.2f}'+'_M13.48_Nk50.dat', skiprows=1)
+#    )
     axioncamb_data_eulbias.append(
-        np.loadtxt(rfpath_outputsuffix+'bias_Euler_z'+f'{redshift:.2f}'+'_M13.48_Nk50.dat', skiprows=1)
+        np.loadtxt(rfpath_outputsuffix+'bias_Euler_z'+f'{redshift:.2f}'+'_M'
+                   +f'{13-np.log10(h_lcdm):.2f}'
+                   +'_Nk50.dat', skiprows=1)
     )
     axioncamb_data_lagbias.append(
-        np.loadtxt(rfpath_outputsuffix+'bias_Lagrangian_z'+f'{redshift:.2f}'+'_M13.48_Nk50.dat', skiprows=1)
+        np.loadtxt(rfpath_outputsuffix+'bias_Lagrangian_z'+f'{redshift:.2f}'+'_M'
+                   +f'{13-np.log10(h_lcdm):.2f}'
+                   +'_Nk50.dat', skiprows=1)
     )
     axioncamb_data_tf.append(
         np.loadtxt(rfpath_outputsuffix+'z'+f'{redshift:.2f}'+'TF_CAMB.dat', skiprows=1)
@@ -118,11 +130,12 @@ for m_idx, m_val in enumerate(Mnu):
     
     os.system('mv ./run.ini ./run_axioncamb_'+str(m_idx)+'.ini')
 
-kplot = np.geomspace(10**-4, 10**0, 41)
+kplot = np.geomspace(10**-4, 10**0, 41) #Units: h Mpc^-1
 kfs = np.array([0.08*(mval/3./0.1)*h[m_idx]/np.sqrt(1+redshift) 
     for midx, mval in enumerate(Mnu)])
 
 colors = sns.color_palette("seismic", len(Mnu))
+
 
 plt.figure(figsize=(15, 7.5))
 plt.xscale('log')
@@ -167,16 +180,29 @@ for m_idx, m_val in enumerate(Mnu):
     plt.plot(
         kplot, 
         yplot, 
-        label=r'$\Sigma m_\nu$ = '+f'{Mnu[m_idx]:.2f}', 
+        label=r'Nick Data, $\Sigma m_\nu$ = '+f'{Mnu[m_idx]:.2f}', 
         color=colors[m_idx], 
     )
+
+#    plt.plot(
+#        kplot, 
+#        eulbiasplot/eulbiasplot[0], 
+#        label=r'$\Sigma m_\nu$ = '+f'{Mnu[m_idx]:.2f}', 
+#        color=colors[m_idx], 
+#    )
+#    plt.plot(
+#        kplot, 
+#        reco_eulbiasplot/reco_eulbiasplot[0], 
+#        color=colors[m_idx], 
+#        linestyle='dashed', 
+#        linewidth=5
+#    )
 
 
 plt.xlabel(r'$k ~[{\rm Mpc}^{-1}]$', fontsize=15)
 plt.ylabel(r'$((b_1(k)/b_1(k_{\rm ref}))_{reconstructed} - (b_1(k)/b_1(k_{\rm ref}))_{relicfastoutput})/(b_1(k)/b_1(k_{\rm ref}))_{relicfastoutput}$', fontsize=15)
-plt.title(r'$b_1 = (1+b_1^L)(\mathcal{T}_{cb}/\mathcal{T}_m)$', fontsize=15)
+#plt.title(r'1805.11623 Fig 10, axionCAMB, $b_1 = (1+b_1^L)(\mathcal{T}_{cb}/\mathcal{T}_m)$', fontsize=15)
+plt.title(r'1805.11623 Fig 10, axionCAMB, $b_1 = (1+b_1^L)(\mathcal{T}_{cb}/\mathcal{T}_m)$', fontsize=15)
 plt.legend(fontsize=15)
 plt.grid(True, which='both', axis='both')  
 plt.savefig("RelicFast_Lagrangian_to_Eulerian_Bias.png")
-
-
